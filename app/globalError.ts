b64e01786fc23cc9e2c6661a6bd5daacff3d38a3
morshedulmunna <morshedulmunna1@gmail.com>
2023-12-12 00:59:27 +0600

    add/ setup global error handler

---
+++ app/globalError.ts
@@ -0,0 +1,25 @@
+import {NextFunction, Request, Response} from "express";
+
+export const notFoundHandler = (
+    _req: Request,
+    _res: Response,
+    next: NextFunction
+) => {
+    const error: any = new Error("Resource Not Found");
+    error.status = 404;
+    next(error);
+};
+
+export const errorHandler = (
+    error: any,
+    _req: Request,
+    res: Response,
+    _next: NextFunction
+) => {
+    if (error.status) {
+        return res.status(error.status).json({
+            message: error.message,
+        });
+    }
+    res.status(500).json({message: "Something went Wrong"});
+};
