df7e98398d55c07aa7fa89375d614ae82bd2a75e
morshedulmunna <morshedulmunna1@gmail.com>
2023-12-15 00:56:40 +0600

    add:/  export from all service in the index.ts file

---
+++ services/index.ts
@@ -0,0 +1,4 @@
+export {activateUser} from "./user/activate.controller";
+export {loginUser} from "./user/login.controller";
+export {logout} from "./user/logout.controller";
+export {register} from "./user/register.controller";
