b7741acf15380ecb2eeec8f887dfa9cfde55ea50
morshedulmunna <morshedulmunna1@gmail.com>
2023-12-15 00:56:56 +0600

    update:/ path update

--- routes/user.route.ts
+++ routes/user.route.ts
@@ -1,12 +1,11 @@
 import express from "express";
-import {activateUser} from "../services/user/activate.controller";
-import {loginUser} from "../services/user/login.controller";
-import {register} from "../services/user/register.controller";
+import {activateUser, loginUser, logout, register} from "../services";
 
 const userRouter = express.Router();
 
 userRouter.post("/registration", register);
 userRouter.post("/activate-user", activateUser);
 userRouter.post("/login", loginUser);
+userRouter.get("/logout", logout);
 
 export default userRouter;
