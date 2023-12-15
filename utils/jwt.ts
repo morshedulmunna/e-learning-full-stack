86ed2beb46abdc55913dde3076e0adf20916af5c
morshedulmunna <morshedulmunna1@gmail.com>
2023-12-15 12:52:59 +0600

    fix:/ fix time calculation and export options

--- utils/jwt.ts
+++ utils/jwt.ts
@@ -3,36 +3,36 @@ import {redis} from "../config/redis.config";
 import {iTokenOption} from "../types/jwt.type";
 import {iUser} from "../types/user.type";
 
+// Parse environment variables to integer with fallback values
+const accessTokenExpire = parseInt(
+    process.env.ACCESS_TOKEN_EXPIRE || "300",
+    10
+);
+const refreshTokenExpire = parseInt(
+    process.env.REFRESH_TOKEN_EXPIRE || "1200",
+    10
+);
+
+// Option for cookies
+export const accessTokenOptions: iTokenOption = {
+    expires: new Date(Date.now() + accessTokenExpire * 60 * 60 * 1000),
+    maxAge: refreshTokenExpire * 60 * 60 * 1000,
+    httpOnly: true,
+    sameSite: "lax",
+};
+export const refreshTokenOptions: iTokenOption = {
+    expires: new Date(Date.now() + refreshTokenExpire * 24 * 60 * 60 * 1000),
+    maxAge: refreshTokenExpire * 24 * 60 * 60 * 1000,
+    httpOnly: true,
+    sameSite: "lax",
+};
+
 export const sendToken = (user: iUser, statusCode: number, res: Response) => {
     const accessToken = user.signAccessToken();
     const refreshToken = user.signRefreshToken();
 
     // Upload the session to redis database for using caching
     redis.set(user._id, JSON.stringify(user));
-
-    // Parse environment variables to integer with fallback values
-    const accessTokenExpire = parseInt(
-        process.env.ACCESS_TOKEN_EXPIRE || "300",
-        10
-    );
-    const refreshTokenExpire = parseInt(
-        process.env.REFRESH_TOKEN_EXPIRE || "1200",
-        10
-    );
-
-    // Option for cookies
-    const accessTokenOptions: iTokenOption = {
-        expires: new Date(Date.now() + accessTokenExpire * 1000),
-        maxAge: refreshTokenExpire * 1000,
-        httpOnly: true,
-        sameSite: "lax",
-    };
-    const refreshTokenOptions: iTokenOption = {
-        expires: new Date(Date.now() + refreshTokenExpire * 1000),
-        maxAge: refreshTokenExpire * 1000,
-        httpOnly: true,
-        sameSite: "lax",
-    };
 
     if (process.env.NODE_ENV === "production") {
         accessTokenOptions.secure = true;
