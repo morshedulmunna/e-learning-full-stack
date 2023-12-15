e8765804164177a9e82563aa420cd937657ddd2a
morshedulmunna <morshedulmunna1@gmail.com>
2023-12-15 12:52:33 +0600

    update:/ update access token route

--- routes/user.route.ts
+++ routes/user.route.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import {isAuthenticated} from "../middleware/authentication";
 import {activateUser, loginUser, logout, register} from "../services";
+import {updateAccessToken} from "../services/auth/refresh.controller";
 
 const userRouter = express.Router();
 
@@ -8,5 +9,6 @@ userRouter.post("/registration", register);
 userRouter.post("/activate-user", activateUser);
 userRouter.post("/login", loginUser);
 userRouter.get("/logout", isAuthenticated, logout);
+userRouter.get("/refresh-token", updateAccessToken);
 
 export default userRouter;
