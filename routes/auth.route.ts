4397c4197056386c17d2fc8a384b8c1cb106b131
morshedulmunna <morshedulmunna1@gmail.com>
2023-12-15 16:38:02 +0600

    update:/ user info route

--- routes/auth.route.ts
+++ routes/auth.route.ts
@@ -2,6 +2,7 @@ import express from "express";
 import {isAuthenticated} from "../middleware/authentication";
 import {activateUser, loginUser, logout, register} from "../services";
 import {updateAccessToken} from "../services/auth/refresh.controller";
+import {getUserInfo} from "../services/users/user.controller";
 
 const userRouter = express.Router();
 
@@ -10,5 +11,6 @@ userRouter.post("/activate-user", activateUser);
 userRouter.post("/login", loginUser);
 userRouter.get("/logout", isAuthenticated, logout);
 userRouter.get("/refresh-token", updateAccessToken);
+userRouter.get("/user-info", isAuthenticated, getUserInfo);
 
 export default userRouter;
