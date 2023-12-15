880f2114321dc18994b57b460488b13c96cc1c9b
morshedulmunna <morshedulmunna1@gmail.com>
2023-12-15 16:40:23 +0600

    router setup for social auth

--- routes/auth.route.ts
+++ routes/auth.route.ts
@@ -2,6 +2,7 @@ import express from "express";
 import {isAuthenticated} from "../middleware/authentication";
 import {activateUser, loginUser, logout, register} from "../services";
 import {updateAccessToken} from "../services/auth/refresh.controller";
+import {socialAuth} from "../services/auth/social.auth.service";
 import {getUserInfo} from "../services/users/user.controller";
 
 const userRouter = express.Router();
@@ -12,5 +13,6 @@ userRouter.post("/login", loginUser);
 userRouter.get("/logout", isAuthenticated, logout);
 userRouter.get("/refresh-token", updateAccessToken);
 userRouter.get("/user-info", isAuthenticated, getUserInfo);
+userRouter.post("/social-auth", socialAuth);
 
 export default userRouter;
