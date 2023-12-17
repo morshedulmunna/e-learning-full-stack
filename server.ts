3f60af21d032f75b5cd68ef3ac2f01c87625c359
morshedulmunna <morshedulmunna1@gmail.com>
2023-12-17 16:23:48 +0600

    update:/ refactor code

--- server.ts
+++ server.ts
@@ -2,7 +2,11 @@ require("dotenv").config();
 import {app} from "./app/app";
 import connectDB from "./config/db.config";
 
+(function () {
+    // Database calling
+    connectDB();
+})();
+
 app.listen(process.env.PORT, () => {
     console.log(`server listening on port ${process.env.PORT}`);
-    connectDB();
 });
